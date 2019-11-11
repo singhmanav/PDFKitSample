2c6ab8eb8279f920c1cb86aa93d60339357f7d9d
Manav <manavmanuprakash@gmail.com>
2019-11-11 12:04:06 +0530

    Update ViewController.swift

--- PDFKitSample/ViewController.swift
+++ PDFKitSample/ViewController.swift
@@ -51,7 +51,7 @@ fileprivate func convertFromUIPageViewControllerOptionsKey(_ input: UIPageViewCo
 }
 
 public extension UIDevice {
-    
+
     static let modelName: String = {
         var systemInfo = utsname()
         uname(&systemInfo)
@@ -60,12 +60,13 @@ public extension UIDevice {
             guard let value = element.value as? Int8, value != 0 else { return identifier }
             return identifier + String(UnicodeScalar(UInt8(value)))
         }
-        
+
         func mapToDevice(identifier: String) -> String { // swiftlint:disable:this cyclomatic_complexity
             #if os(iOS)
             switch identifier {
-            case "iPod5,1":                                 return "iPod Touch 5"
-            case "iPod7,1":                                 return "iPod Touch 6"
+            case "iPod5,1":                                 return "iPod touch (5th generation)"
+            case "iPod7,1":                                 return "iPod touch (6th generation)"
+            case "iPod9,1":                                 return "iPod touch (7th generation)"
             case "iPhone3,1", "iPhone3,2", "iPhone3,3":     return "iPhone 4"
             case "iPhone4,1":                               return "iPhone 4s"
             case "iPhone5,1", "iPhone5,2":                  return "iPhone 5"
@@ -84,17 +85,23 @@ public extension UIDevice {
             case "iPhone11,2":                              return "iPhone XS"
             case "iPhone11,4", "iPhone11,6":                return "iPhone XS Max"
             case "iPhone11,8":                              return "iPhone XR"
+            case "iPhone12,1":                              return "iPhone 11"
+            case "iPhone12,3":                              return "iPhone 11 Pro"
+            case "iPhone12,5":                              return "iPhone 11 Pro Max"
             case "iPad2,1", "iPad2,2", "iPad2,3", "iPad2,4":return "iPad 2"
-            case "iPad3,1", "iPad3,2", "iPad3,3":           return "iPad 3"
-            case "iPad3,4", "iPad3,5", "iPad3,6":           return "iPad 4"
+            case "iPad3,1", "iPad3,2", "iPad3,3":           return "iPad (3rd generation)"
+            case "iPad3,4", "iPad3,5", "iPad3,6":           return "iPad (4th generation)"
+            case "iPad6,11", "iPad6,12":                    return "iPad (5th generation)"
+            case "iPad7,5", "iPad7,6":                      return "iPad (6th generation)"
+            case "iPad7,11", "iPad7,12":                    return "iPad (7th generation)"
             case "iPad4,1", "iPad4,2", "iPad4,3":           return "iPad Air"
             case "iPad5,3", "iPad5,4":                      return "iPad Air 2"
-            case "iPad6,11", "iPad6,12":                    return "iPad 5"
-            case "iPad7,5", "iPad7,6":                      return "iPad 6"
-            case "iPad2,5", "iPad2,6", "iPad2,7":           return "iPad Mini"
-            case "iPad4,4", "iPad4,5", "iPad4,6":           return "iPad Mini 2"
-            case "iPad4,7", "iPad4,8", "iPad4,9":           return "iPad Mini 3"
-            case "iPad5,1", "iPad5,2":                      return "iPad Mini 4"
+            case "iPad11,4", "iPad11,5":                    return "iPad Air (3rd generation)"
+            case "iPad2,5", "iPad2,6", "iPad2,7":           return "iPad mini"
+            case "iPad4,4", "iPad4,5", "iPad4,6":           return "iPad mini 2"
+            case "iPad4,7", "iPad4,8", "iPad4,9":           return "iPad mini 3"
+            case "iPad5,1", "iPad5,2":                      return "iPad mini 4"
+            case "iPad11,1", "iPad11,2":                    return "iPad mini (5th generation)"
             case "iPad6,3", "iPad6,4":                      return "iPad Pro (9.7-inch)"
             case "iPad6,7", "iPad6,8":                      return "iPad Pro (12.9-inch)"
             case "iPad7,1", "iPad7,2":                      return "iPad Pro (12.9-inch) (2nd generation)"
@@ -116,8 +123,8 @@ public extension UIDevice {
             }
             #endif
         }
-        
+
         return mapToDevice(identifier: identifier)
     }()
-    
+
 }
